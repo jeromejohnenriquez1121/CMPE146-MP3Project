80ecef58830bb20dfab3d82a8b973cfc5e06bde2
Jerome Enriquez <jeromej.enriquez@gmail.com>
2021-11-08 17:12:26 -0800

    Fixed cli function

--- Milestone 1/milestone1.c
+++ Milestone 1/milestone1.c
@@ -1,3 +1,5 @@
+//main.c
+
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -19,7 +21,7 @@ QueueHandle_t Q_songname;
 QueueHandle_t Q_songdata;
 
 static void open_file(FIL *file, songname_t *name) { f_open(&file, &name[0], FA_READ); }
-static void read_file(FIL *file, char *bytes) { f_read(&file, &bytes, 512, bytes_read); }
+static void read_file(FIL *file, char *bytes) { f_read(&file, &bytes[0], sizeof(&bytes[0]), &bytes_read); }
 static void close_file(FIL *this_file) { f_close(&this_file); }
 
 // Reader tasks receives song-name over Q_songname to start reading it
@@ -31,19 +33,19 @@ void mp3_reader_task(void *p)
   {
     printf("Waiting for song to receive.\n");
     xQueueReceive(Q_songname, &name[0], portMAX_DELAY);
-    printf("Received song to play: %s.\n", name);
+    printf("Received song to play: %s.\n", &name[0]);
 
     FIL *file;
     open_file(&file, &name[0]);
-    printf("Opened file.\n");
+    printf("Opened file: %s.\n", &name[0]);
     while (bytes_read != 0)
     {
-      read_file(file, &name[0]);
+      read_file(&file, &name[0]);
       xQueueSend(Q_songdata, &bytes_512[0], portMAX_DELAY);
-      printf("Sent 512 bytes to queue.\n");
+      printf("Sent 512 bytes.\n");
     }
-    close_file(file);
-    printf("Closed file.\n");
+    close_file(&file);
+    printf("Closed file: %s.\n", &name[0]);
   }
 }
 
@@ -74,4 +76,22 @@ void main(void)
   // xTaskCreate(mp3_player_task, "Play mp3", 4096 / sizeof(void *), NULL, PRIORITY_HIGH, NULL);
 
   vTaskStartScheduler();
+}
+
+/*---------------------------------------------------------------------------------------------------*/
+// handers_general.c
+
+extern QueueHandle_t Q_songname;
+
+app_cli_status_e cli__play_song(app_cli__argument_t argument, sl_string_s user_input_minus_command_name,
+                                app_cli__print_string_function cli_output)
+{
+  // user_input_minus_command_name is actually a 'char *' pointer type
+  // We tell the Queue to copy 32 bytes of songname from this location
+  sl_string__erase_first_word(user_input_minus_command_name, ' ');
+
+  xQueueSend(Q_songname, &user_input_minus_command_name, portMAX_DELAY);
+
+  printf("Sent %s over to the Q_songname\n", user_input_minus_command_name.cstring);
+  return APP_CLI_STATUS__SUCCESS;
 }
