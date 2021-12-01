61873f9a71dc9f45981811262dc03498615dba13
Jerome Enriquez <jeromej.enriquez@gmail.com>
2021-11-30 16:20:51 -0800

    Added ssp0_mp3.h

---
+++ projects/lpc40xx_freertos/mp3_project/ssp0_mp3.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#pragma once
+
+void ssp0_mp3__init(void);
+
+uint8_t ssp0_mp3__send_byte(uint8_t byte_to_transfer);
+
+void ssp0_mp3__set_prescalar_registers(uint32_t max_clock_mhz);
